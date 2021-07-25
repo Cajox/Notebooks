795162af9b067583037b6ee0278577359f499141
Cajox <j.miljkovic95@gmail.com>
2021-07-25 12:25:07 +0200

    Feat: Notebook - Specifications relation

--- app/Models/Notebook.php
+++ app/Models/Notebook.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Notebook extends Model
 {
@@ -18,4 +19,14 @@ class Notebook extends Model
         'search_data',
         'price'
     ];
+
+    /**
+     * Notebook specifications
+     *
+     * @return HasMany
+     */
+    public function specifications()
+    {
+        return $this->hasMany(Specification::class);
+    }
 }
